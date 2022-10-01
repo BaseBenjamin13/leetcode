d6e7872402ab08d56356ff39099043acabf09900
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2022-09-30 22:05:55 -0400

    Time: 49 ms (99.73%), Space: 41.4 MB (98.97%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -4,12 +4,12 @@
  */
 var lengthOfLastWord = function(s) {
     let words = s.split(/[\s  ]+/);
-    let lengthOfLastWord;
+
     if(words[words.length - 1].length === 0){
-        lengthOfLastWord = words[words.length - 2].length;
+        return words[words.length - 2].length;
     }else {
-        lengthOfLastWord = words[words.length - 1].length;
+        return words[words.length - 1].length;
     }
     
-    return lengthOfLastWord;
+
 };
