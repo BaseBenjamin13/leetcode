11fbd4f33133a8f2837ef2190209bfdf40462bf3
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2022-11-30 21:03:42 -0500

    Time: 68 ms (92.47%), Space: 42.2 MB (86.12%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function(nums) {
+    let smallNum = Math.min(...nums)
+    let bigNum = Math.max(...nums)
+   
+    for(let i = smallNum; i >= 1; i--){
+        if(bigNum / i % 1 == 0 && smallNum / i % 1 == 0) return i;
+    }
+    return 1;
+};
