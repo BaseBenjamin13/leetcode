5fc909a03307798290ea2ef11f55388c239def21
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2022-12-03 22:07:43 -0500

    Time: 71 ms (99.05%), Space: 45.2 MB (61.57%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -5,7 +5,7 @@
 var getConcatenation = function(nums) {
     let conNums = [...nums]
     for (let i=0; i < nums.length; i++){
-        conNums.push(nums[i]);
+        conNums[i + nums.length] = nums[i];
     }
     return conNums;
 }
