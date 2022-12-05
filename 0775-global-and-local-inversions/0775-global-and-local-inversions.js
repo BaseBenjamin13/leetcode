a01682fa3d07abd89ae88bff8c15fb17fab31105
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2022-12-05 00:30:22 -0500

    Time: 82 ms (100.00%), Space: 51.8 MB (95.24%) - LeetHub

--- 0775-global-and-local-inversions/0775-global-and-local-inversions.js
+++ 0775-global-and-local-inversions/0775-global-and-local-inversions.js
@@ -5,7 +5,10 @@
 var isIdealPermutation = function(nums) {
     let max = -1
     for(let i=0; i < nums.length -2; i++){
-        max = Math.max(nums[i], max)
+        // max = Math.max(nums[i], max)
+        if(nums[i] >= max){
+            max = nums[i];
+        }
         if(max > nums[i+2]) return false
     }
     return true;
