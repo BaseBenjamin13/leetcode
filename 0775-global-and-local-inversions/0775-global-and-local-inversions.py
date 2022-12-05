9360f008c928aa6ae90e64ef56062a978e75de4a
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2022-12-05 00:28:40 -0500

    Time: 905 ms (20.51%), Space: 27.7 MB (13.68%) - LeetHub

--- 0775-global-and-local-inversions/0775-global-and-local-inversions.py
+++ 0775-global-and-local-inversions/0775-global-and-local-inversions.py
@@ -2,10 +2,11 @@ class Solution:
     def isIdealPermutation(self, nums: List[int]) -> bool:
         maxNum = -1
         i = 0
-        while i < len(nums) -2 :
-            maxNum = max(nums[i], maxNum)
-            
+        while i < len(nums) -2:
+            # maxNum = max(nums[i], maxNum) 
+            if nums[i] >= maxNum:
+                maxNum = nums[i]
             if maxNum > nums[i+2]:
                 return False
-            i = i + 1
+            i += 1
         return True
