8a07e5bfd3a7b4fd8e91025e04d19b1b04328f24
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2022-12-13 23:35:25 -0500

    Time: 500 ms (13.33%), Space: 45.1 MB (58.27%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -5,16 +5,16 @@
 var singleNumber = function(nums) {
     let seen = []
     let sec = []
-    nums.forEach(function(num) {
-        if(!seen.includes(num)){
-            seen.push(num)   
+    for(let i=0; i < nums.length; i++) {
+        if(!seen.includes(nums[i])){
+            seen.push(nums[i])   
         } else {
-            sec.push(num)
+            sec.push(nums[i])
         }
-    });
-    for(let i = 0; i < nums.length; i++){
-        if(!sec.includes(nums[i])){
-            return nums[i]
+    }
+    for(let j = 0; j < nums.length; j++){
+        if(!sec.includes(nums[j])){
+            return nums[j]
         }
     }
 };
