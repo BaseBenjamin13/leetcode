f6b8bf0af79078e36d7456e0cd2bd0c15b1bb386
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-01-21 22:28:48 -0500

    Time: 4713 ms (5.00%), Space: 48.9 MB (89.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,13 +4,21 @@
  */
 var containsDuplicate = function(nums) {
     let arr = nums
-    function checkIfDuplicateExists(arr) {
-        return new Set(arr).size !== arr.length
+    // function checkIfDuplicateExists(arr) {
+    //     return new Set(arr).size !== arr.length
+    // }
+    // if(checkIfDuplicateExists(arr)){
+    //     return true
+    // }
+    // else {
+    //     return false
+    // }
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if(nums[i] === nums[j]){
+                return true;
+            }
+        }
     }
-    if(checkIfDuplicateExists(arr)){
-        return true
-    }
-    else {
-        return false
-    }
+    return false
 };
