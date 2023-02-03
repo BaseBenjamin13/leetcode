1ba0488ee1cb4748c1d7ebfed294f809f3159fa4
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-02-02 23:43:18 -0500

    Time: 82 ms (30.42%), Space: 47.4 MB (5.20%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,13 +3,19 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
+    // const arr = [];
+    // for(let i = 0; i < nums.length; i++){
+    //     let temp = 0;
+    //     for(let j = 0; j <= i; j++){
+    //         temp += nums[j]
+    //     }
+    //     arr.push(temp);
+    // }
+    // return arr
     const arr = [];
     for(let i = 0; i < nums.length; i++){
-        let temp = 0;
-        for(let j = 0; j <= i; j++){
-            temp += nums[j]
-        }
-        arr.push(temp);
+        // console.log(nums.slice(0,i+1))
+        arr.push(nums.slice(0,i+1).reduce((a, b) => a + b, 0));
     }
     return arr
 };
