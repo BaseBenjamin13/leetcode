e1d39574e21a76ae84f070dff5fb5a00d9bc016e
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-02-25 12:16:54 -0500

    Time: 42 ms (21.15%), Space: 13.9 MB (47.07%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.py
+++ 2315-count-asterisks/2315-count-asterisks.py
@@ -2,9 +2,11 @@ class Solution:
     def countAsterisks(self, s: str) -> int:
         pipe = 0
         asterisks = 0
-        for i in range(len(s)) :
+        i = 0
+        while i < len(s) :
             if s[i] == '|' :
                 pipe += 1
             if s[i] == '*' and pipe % 2 == 0 :
-                asterisks += 1    
+                asterisks += 1 
+            i += 1
         return asterisks
