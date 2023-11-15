93e32ea4906c296c621bd2a204bf04c6d037df2f
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-11-14 20:16:42 -0500

    Time: 50 ms (60.82%), Space: 41.9 MB (27.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,8 +4,10 @@
  */
 var climbStairs = function(n) {
     let arr=[0,1,2];
-    for(let i=3;i<=n;i++){
+    let i = 3
+    while(i <= n){
         arr[i]=arr[i-1]+arr[i-2];
+        i++
     }
     return arr[n];
 };
